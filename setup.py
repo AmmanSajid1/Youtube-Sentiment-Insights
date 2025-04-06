ce2ee9a9849a8d4faa6a2a75347410456bcb754f
AmmanSajid1 <ammansajid1@gmail.com>
2025-04-06 22:31:02 +0100

    Added data ingestion and data preprocessing notebooks

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Youtube-Sentiment-Insights-Project',
+    version= '0.0.0',
+    author= 'Amman Sajid',
+    author_email= 'ammansajid1@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
